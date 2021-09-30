424b11e978227e677b2e2625726b83c583104f43
Luke Hewitt <insperatum@gmail.com>
2021-09-29 22:41:00 -0400

    Update jupyter_notebook_config.py

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -4,5 +4,5 @@
 # we have SSL certificates enabled for encryption purposes.
 c.NotebookApp.ip = "*"
 c.NotebookApp.open_browser = False
-c.NotebookApp.password="argon2:$argon2id$v=19$m=10240,t=10,p=8$W2kO0XkNgFo8hhjmIQnR2A$IiZ95dzmccQGgkDv783cDQ"
+c.NotebookApp.password="argon2:$argon2id$v=19$m=10240,t=10,p=8$p2L9iXIBvzkuW7PtUuswyg$XlD8gVHaFkqwsD9NevGOJw"
 c.NotebookApp.token=""
